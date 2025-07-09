5b9b84bf9fc6093e56934e715e4075d1f0d2db94
Danqing Wang <danqingwang@berkeley.edu>
2025-07-09 00:02:50 -0700

    add BPE tokenizer 1/n

--- cs336_basics/pretokenization_example.py
+++ cs336_basics/pretokenization_example.py
@@ -1,5 +1,11 @@
+from functools import reduce
 import os
-from typing import BinaryIO
+from typing import BinaryIO, Counter
+import regex
+import sys
+
+special_tokens = ["<|endoftext|>"]
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
 
 def find_chunk_boundaries(
@@ -48,15 +54,50 @@ def find_chunk_boundaries(
     # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
-
-## Usage
-with open(..., "rb") as f:
-    num_processes = 4
-    boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
-
-    # The following is a serial implementation, but you can parallelize this
-    # by sending each start/end pair to a set of processes.
-    for start, end in zip(boundaries[:-1], boundaries[1:]):
+def process_chunk(arg: tuple):
+    start, end, filename = arg
+    local_counter = Counter()
+    with open(filename, 'rb') as f:
         f.seek(start)
         chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        # Run pre-tokenization on your chunk and store the counts for each pre-token
+        for text in regex.split("|".join(map(regex.escape, special_tokens)), chunk):
+                for match in regex.finditer(PAT, text):
+                    token = match.group()
+                    local_counter[token] += 1
+    return local_counter
+## Usage
+if __name__ == "__main__":
+    from multiprocessing import Pool
+    filename = sys.argv[1] if len(sys.argv) > 1 else "test.txt"
+    max_vocab = sys.argv[2] if len(sys.argv) > 2 else 1000
+    num_processes = 4
+    with open(filename, "rb") as f:
+        boundaries = find_chunk_boundaries(
+            f, num_processes, "<|endoftext|>".encode("utf-8"))
+            
+    # The following is a serial implementation, but you can parallelize this 
+    # by sending each start/end pair to a set of processes.
+
+    with Pool(num_processes) as p:
+        counters = p.map(process_chunk, [(0 if i == 0 else boundaries[i-1], boundaries[i], filename) for i in range(len(boundaries))])
+    total = reduce(lambda x, y: x + y, counters)
+        
+    pairs = Counter()
+    vocab = set()
+    for word, frequencey in total:
+        bytes = list(word.encode("utf-8"))
+        for i in range(len(bytes) - 1):
+            vocab.add(bytes[i])
+            pairs[(bytes[i], bytes[i + 1])] += frequencey
+        vocab.add(bytes[-1])
+
+    for special_token in special_tokens:
+         if special_token not in vocab:
+              vocab[special_token] = len(vocab)
+    merge = []
+    while len(vocab) < max_vocab:
+         a,b = max(pairs.items(), key=lambda x: (x[1], x[0]))[0]
+         vocab.add(a+b)
+         merge.append((a,b))
+         del pairs (a,b)
+
